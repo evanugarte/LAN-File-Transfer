9f3b66b78bd0dcaa9dc9312849408950f7427a2a
Evan Ugarte <evanuxd@gmail.com>
2020-04-13 11:31:45 -0700

    Add Simple Download Functionality
    Time to learn more about what a FS object has and actually give this thing a
    UI.

--- imports/ui/DownloadLayout.js
+++ imports/ui/DownloadLayout.js
@@ -1,8 +1,16 @@
 import { Template } from 'meteor/templating';
+import { UploadedFiles } from '../api/uploadedFiles.js';
 
 import './DownloadLayout.html';
 
 Template.DownloadLayout.onCreated(function helloOnCreated() {
   console.log('the deliciousness has landed but now in download');
+  console.log(UploadedFiles.find());
+  
 });
 
+Template.FileList.helpers({
+  uploadedFiles: function () {
+    return UploadedFiles.find();
+  }
+});
