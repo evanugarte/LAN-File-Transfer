b7e4437eb0deb2c17dd28dc356b283e60b3392b4
Evan Ugarte <evanuxd@gmail.com>
2020-04-27 22:52:52 -0700

    Add Icon for Specific File Type

--- imports/ui/DownloadLayout.js
+++ imports/ui/DownloadLayout.js
@@ -14,3 +14,39 @@ Template.FileList.helpers({
     return UploadedFiles.find();
   }
 });
+
+function getFileType(fileName) {
+  let fileType = null;
+  const splitFileName = fileName.split(".");
+  if (splitFileName.length > 1) {
+    fileType = splitFileName[splitFileName.length - 1];
+  }
+  return fileType;
+}
+
+const FILE_MAP = {
+  'pdf': 'file alternate',
+  'doc': 'file alternate',
+  'docx': 'file alternate',
+  'md': 'file alternate',
+  'c': 'file code',
+  'py': 'file code',
+  'cpp': 'file code',
+  'js': 'file code',
+  'html': 'file code',
+  'css': 'file code',
+  'mp3': 'headphones',
+  'mp4': 'film',
+  'avi': 'film',
+}
+
+Template.File.helpers({
+  getFileIcon: function(file) {
+    const fileType = getFileType(file.original.name);
+    if(fileType && FILE_MAP[fileType]) {
+      return FILE_MAP[fileType];
+    } else {
+      return 'file';
+    }
+  }
+});
