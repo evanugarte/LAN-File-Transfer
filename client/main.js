f6d345cf1363625cca3198779b48e59122958a7a
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2020-10-24 12:37:37 -0700

    Remove Meteor Code (#28)
    * Remove Meteor Code
    
    * remove .meteor

--- client/main.js
+++
@@ -1,9 +0,0 @@
-import { Template } from 'meteor/templating';
-import '../imports/routes'
-import '../imports/ui/SideMenu'
-
-import './main.html';
-
-Template.body.onCreated(function helloOnCreated() {
-  // counter starts at 0
-});
