e36fe00d8a4f937a94a75c96384931fbb83a2873
Evan Ugarte <evanuxd@gmail.com>
2020-11-19 23:48:13 -0800

    Make API Gateway Code Object Oriented

--- api-gateway/main.js
+++ api-gateway/main.js
@@ -1,66 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const fetch = require('node-fetch');
+const { ApiGatewayServer } = require('./src/ApiGatewayServer');
 
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload({
-  createParentPath: true
-}));
-
-const SERVER_PORT = process.env.SERVER_PORT || 5000;
-const CPP_SERVICE_URL = process.env.CPP_SERVICE_URL || 'http://localhost:5001';
-
-function attemptUpload(fileData) {
-  return new Promise((resolve, reject) => {
-    fetch(`${CPP_SERVICE_URL}/upload`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: fileData
-    })
-      .then(res => res.json())
-      .then(data => {
-        resolve(data);
-      })
-      .catch(err => {
-        reject('upload failed:', err);
-      });
-  });
+function main() {
+  let apiGatewayServer = new ApiGatewayServer();
+  apiGatewayServer.startServer();
 }
 
-function attemptDownload(fileName) {
-  return new Promise((resolve, reject) => {
-    fetch(`${CPP_SERVICE_URL}/download/${fileName}`)
-      .then(res => resolve(res))
-      .catch(err => {
-        reject('download failed:', err);
-      });
-  });
-}
-
-app.post('/upload', async (req, res) => {
-  const fileData = req.files['fileToUpload'].data;
-  const uploadServiceResponse = await attemptUpload(fileData);
-  // todo, see https://github.com/evanugarte/LAN-File-Transfer/issues/32
-  // we will use uploadServiceResponse to save file metadata
-  console.log('server responded with:', uploadServiceResponse);
-  res.send(uploadServiceResponse);
-});
-
-
-app.get('/download', async (req, res) => {
-  const { fileId } = req.query;
-  const downloadServiceResponse = await attemptDownload(fileId);
-  downloadServiceResponse.body.pipe(res);
-});
-
-app.listen(SERVER_PORT, () =>
-  console.log(`App is listening on port ${SERVER_PORT}.`)
-);
+main();
---
+++ api-gateway/src/FileApiHandler.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+const CPP_SERVICE_URL = process.env.CPP_SERVICE_URL || 'http://localhost:5001';
+
+class FileApiHandler {
+  attemptUpload(fileData) {
+    return new Promise((resolve, reject) => {
+      fetch(`${CPP_SERVICE_URL}/upload`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: fileData
+      })
+        .then(res => res.json())
+        .then(data => {
+          resolve(data);
+        })
+        .catch(err => {
+          reject('upload failed:', err);
+        });
+    });
+  }
+  
+  attemptDownload(fileName) {
+    return new Promise((resolve, reject) => {
+      fetch(`${CPP_SERVICE_URL}/download/${fileName}`)
+        .then(res => resolve(res))
+        .catch(err => {
+          reject('download failed:', err);
+        });
+    });
+  }
+}
+
+module.exports = { FileApiHandler };
