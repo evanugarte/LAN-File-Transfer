4f5be34c0f4d399469477217e1514079d6a71633
Evan Ugarte <evanuxd@gmail.com>
2020-12-08 21:10:35 -0800

    Move C++ server logic to lft::HttpServer
    Resolves #56

--- server/include/http_server.h
+++ server/include/http_server.h
@@ -1,13 +1,74 @@
 #pragma once
 
-#include <sys/time.h>
-
-#include <fstream>
 #include <iostream>
 #include <string>
 
 #include "file_handler.h"
+#include "served/multiplexer.hpp"
+#include "served/net/server.hpp"
+#include "SimpleJSON/json.hpp"
 
 namespace lft {
-class HttpServer {};
+constexpr char kUploadEndpoint[] = "/upload";
+constexpr char kDownloadEndpoint[] = "/download/{file_name}";
+constexpr char kDeleteEndpoint[] = "/delete/{file_to_delete}";
+constexpr char kIpAddress[] = "0.0.0.0";
+constexpr char kPort[] = "5001";
+class HttpServer {
+ public:
+  HttpServer(served::multiplexer multiplexer, lft::FileHandler file_handler)
+      : multiplexer(multiplexer), file_handler(file_handler) {}
+
+  auto UploadHandler() {
+    return [&](served::response &response, const served::request &request) {
+      const json::JSON &file_handler_response =
+          file_handler.ParseAndWriteHttpRequestBody(request.body());
+      std::ostringstream stream;
+      stream << file_handler_response;
+      response << stream.str();
+    };
+  }
+
+  auto DownloadHandler() {
+    return [&](served::response &response, const served::request &request) {
+      const std::string &file_name = request.params["file_name"];
+      std::ostringstream buffer;
+      const bool &read_result =
+          file_handler.WriteFileToBuffer(buffer, file_name);
+      if (read_result) {
+        response << buffer.str();
+      } else {
+        served::response::stock_reply(404, response);
+      }
+    };
+  }
+
+  auto DeleteHandler() {
+    return [&](served::response &response, const served::request &request) {
+      const std::string &file_to_delete = request.params["file_to_delete"];
+      const bool &read_result = file_handler.DeleteFile(file_to_delete);
+      if (read_result) {
+        served::response::stock_reply(200, response);
+      } else {
+        served::response::stock_reply(404, response);
+      }
+    };
+  }
+
+  void InitializeEndpoints() {
+    multiplexer.handle(kUploadEndpoint).post(UploadHandler());
+    multiplexer.handle(kDownloadEndpoint).get(DownloadHandler());
+    multiplexer.handle(kDeleteEndpoint).del(DeleteHandler());
+  }
+
+  void StartServer() {
+    served::net::server server(kIpAddress, kPort, multiplexer);
+    std::cout << "Starting server to listen on port 5001..." << std::endl;
+    server.run(10);
+  }
+
+ private:
+  served::multiplexer multiplexer;
+  lft::FileHandler file_handler;
+};
 } // namespace lft
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,49 +1,15 @@
-#include <SimpleJSON/json.hpp>
-#include <iostream>
-#include <string>
-
 #include "file_handler.h"
+#include "http_server.h"
 #include "served/multiplexer.hpp"
 #include "served/net/server.hpp"
 
 int main(int, char **) {
-  served::multiplexer mux;
-  lft::FileHandler fh;
-  mux.handle("/upload").post(
-      [&](served::response &response, const served::request &request) {
-        const json::JSON &file_handler_response =
-            fh.ParseAndWriteHttpRequestBody(request.body());
-        std::ostringstream stream;
-        stream << file_handler_response;
-        response << stream.str();
-      });
+  served::multiplexer multiplexer;
+  lft::FileHandler file_handler;
+  lft::HttpServer http_server(multiplexer, file_handler);
 
-  mux.handle("/download/{file_name}").get(
-      [&](served::response &response, const served::request &request) {
-        const std::string &file_name = request.params["file_name"];
-        std::ostringstream buffer;
-        const bool &read_result = fh.WriteFileToBuffer(buffer, file_name);
-        if (read_result) {
-          response << buffer.str();
-        } else {
-          served::response::stock_reply(404, response);
-        }
-      });
-
-  mux.handle("/delete/{file_to_delete}").del(
-      [&](served::response &response, const served::request &request) {
-        const std::string &file_to_delete = request.params["file_to_delete"];
-        const bool &read_result = fh.DeleteFile(file_to_delete);
-        if (read_result) {
-          served::response::stock_reply(200, response);
-        } else {
-          served::response::stock_reply(404, response);
-        }
-      });
-
-  served::net::server server("0.0.0.0", "5001", mux);
-  std::cout << "Starting server to listen on port 5001..." << std::endl;
-  server.run(10);
+  http_server.InitializeEndpoints();
+  http_server.StartServer();
 
   return (EXIT_SUCCESS);
 }
