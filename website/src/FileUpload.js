2eb48608cc1b3c0b246dc409f3f8ba6c15b7c969
Evan Ugarte <evanuxd@gmail.com>
2021-11-12 01:33:29 -0800

    Confirm file is uploaded
    Resolves #120

--- website/src/FileUpload.js
+++ website/src/FileUpload.js
@@ -1,9 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { handleUpload } from './ApiFunctions/ApiHandler';
 
 export default function FileUpload() {
   const [fileToUpload, setFileToUpload] = useState();
+  const [fileInput, setFileInput] = useState();
+  const [showUploadStatus, setShowUploadStatus] = useState(false);
+  const [uploadStatus, setUploadStatus] = useState("Uploading...");
+  const [showCountdown, setShowCountdown] = useState(false);
+  const [countdown, setCountdown] = useState(10);
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    if (countdown <= 0) {
+      resetFileInfo();
+    }
+  }, [countdown]);
+
+  function resetFileInfo() {
+    clearInterval(intervalRef.current);
+    setShowUploadStatus(false)
+    setShowCountdown(false);
+    setFileToUpload(null);
+    setUploadStatus("Uploading...");
+    setCountdown(10);
+  }
+
   function handleChange(event) {
+    resetFileInfo();
     if (event.target.files[0]) {
       setFileToUpload(event.target.files[0]);
     } else {
@@ -11,18 +34,43 @@ export default function FileUpload() {
     }
   }
 
-  function uploadFile() {
+  async function uploadFile() {
     if (fileToUpload) {
-      handleUpload(fileToUpload);
+      try {
+        setShowUploadStatus(true);
+        await handleUpload(fileToUpload);
+        if (fileInput) {
+          fileInput.value = '';
+        }
+        setUploadStatus("Done!");
+      } catch (error) {
+        setUploadStatus("Failed!");
+      } finally {
+        setShowCountdown(true);
+        intervalRef.current = setInterval(() => setCountdown((countdown) => countdown - 1), 1000);
+      }
     }
   }
+
   return (
     <div>
       <h1>file upload</h1>
-      <div style={{display: 'inline-block'}}>
-        <input onChange={handleChange} type="file" />
+      <div style={{ display: 'inline-block' }}>
+        <input onChange={handleChange} ref={el => setFileInput(el)} type='file' />
         <button disabled={!fileToUpload} onClick={uploadFile}>upload</button>
       </div>
+      {
+        showUploadStatus && <React.Fragment>
+          <p>
+            {fileToUpload.name}: <span id="status">
+              {uploadStatus}
+            </span>
+          </p>
+        </React.Fragment>
+      }
+      {
+        showCountdown && <p>Closing dialog in {countdown} seconds...</p>
+      }
     </div>
   );
 }
